011222e8341d89c8395d5645dfe00a6f69eef22d
Kevin Tan <tankevin@stanford.edu>
2020-08-29 01:33:09 -0400

    📝 Explain why test_fail fails

--- tests/test_expected_failure.py
+++ tests/test_expected_failure.py
@@ -4,4 +4,5 @@ import unittest
 class ExpectedFailureTestCase(unittest.TestCase):
     @unittest.expectedFailure
     def test_fail(self):
+        """This test is supposed to fail so you can see what a failed test looks like!"""
         self.assertEqual(1, 0, "broken")
